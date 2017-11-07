32ec875c83eb159ac5440231149582803906fc5b
Orrielel <orrie.nor@gmail.com>
2017-11-07 03:40:23 +0100

    Create BetterImagePopups.plugin.js

---
+++ Plugins/BetterImagePopups/BetterImagePopups.plugin.js
@@ -0,0 +1,50 @@
+//META{"name":"BetterImagePopups"}*//
+
+/* global BdApi */
+
+class BetterImagePopups {
+	getName() {return "Better Image Popups";}
+	getShortName() {return "BetterImagePopups";}
+	getDescription() {return "Show full sized images in image popup";}
+	getVersion() {return "1.0.0";}
+	getAuthor() {return "Orrie";}
+
+	load() {}
+	start(){
+		BdApi.injectCSS(this.getShortName(), `
+.modal-image.bip-scroller {margin: 0 5px; overflow: auto;}
+.modal-image img {max-width: calc(100vw - 160px); left: 50%; transform: translateX(-50%);}
+.modal-image img.bip-center {max-height: calc(100vh - 120px); max-width: calc(100vw - 160px);}
+.modal-image .download-button {display: block; position: absolute; right: 10px; bottom: 6px; pointer-events: auto; text-transform: capitalize;}
+.modal-image .image.image-loading {opacity: 0.9;}
+.modal-image .image.image-loading::before {background: transparent;}
+/* ImageToClipboard Compatibility*/
+.modal-image span.download-button {right: 100px;}
+.modal-image a:nth-of-type(2) {right: 115px;}
+		`);
+	}
+	stop(){
+		BdApi.clearCSS(this.getShortName());
+	}
+
+	observer(e) {
+		const target = e.target;
+		if (target.classList.contains("modal-image") || !target.classList.contains("bip-scroller")) {
+			const img = target.firstElementChild;
+			if (img.src) {
+				target.classList.add("bip-scroller");
+				img.classList.add("bip-center");
+				img.src = img.src.split("?")[0];
+				img.removeAttribute("width");
+				img.removeAttribute("height");
+				img.onload = function(){
+					if (this.naturalHeight > window.innerHeight*1.35) {
+						this.addEventListener("click", function() {
+							this.classList.toggle("bip-center");
+						}, false);
+					}
+				};
+			}
+		}
+	}
+}
