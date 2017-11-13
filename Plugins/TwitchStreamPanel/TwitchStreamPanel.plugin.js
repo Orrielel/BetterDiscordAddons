6b8356f13efd736a63883b4c56ec64e87134b106
Orrielel <orrie.nor@gmail.com>
2017-11-13 13:40:57 +0100

    Update TwitchStreamPanel.plugin.js

--- Plugins/TwitchStreamPanel/TwitchStreamPanel.plugin.js
+++ Plugins/TwitchStreamPanel/TwitchStreamPanel.plugin.js
@@ -7,7 +7,7 @@ const TwitchStreamPanel = (function() {
 	const script = {
 		name: "Twitch Stream Panel",
 		file: "TwitchStreamPanel",
-		version: "1.2.4",
+		version: "1.2.5",
 		author: "Orrie",
 		desc: "Adds a toggleable panel that gives you stream statuses from Twitch",
 		url: "https://github.com/Orrielel/BetterDiscordAddons/tree/master/Plugins/TwitchStreamPanel",
@@ -51,14 +51,16 @@ const TwitchStreamPanel = (function() {
 .TwitchStreamPanel.orriePluginSettings .orriePluginTable {margin: 0 0 25px;}
 .TwitchStreamPanel.orriePluginSettings .orriePluginTable th {font-weight: 700;}
 .TwitchStreamPanel.orriePluginSettings .orriePluginTable img {height: 20px;}
-.TwitchStreamPanel.orriePluginSettings .orriePluginForm {margin: 0 0 25px;}
-.TwitchStreamPanel.orriePluginSettings .stream_form button {display: table; margin: 5px auto 0;}
-.TwitchStreamPanel.orriePluginSettings .orriePluginStreamlist {background-color: rgba(0, 0, 0, 0.15); border: 1px solid rgba(0, 0, 0, 0.25); border-radius: 5px; margin: 0; max-height: 300px; overflow-x: auto; padding: 15px 0;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginStreamInput button {display: table; margin: 5px auto 0;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginStreamInput td:first-of-type {white-space: nowrap;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginStreamInput td:last-of-type {width: 100%;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginStreamInput input {width: 100%;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginStreamFooter {position: relative;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginStreamFooter span {position: absolute; top: 7px; right: 8px;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginStreamList {background-color: rgba(0, 0, 0, 0.15); border: 1px solid rgba(0, 0, 0, 0.25); border-radius: 5px; margin: 0; max-height: 300px; overflow-x: auto; padding: 15px 0;}
 .TwitchStreamPanel.orriePluginSettings .orriePluginServer {margin-bottom: 10px;}
 .TwitchStreamPanel.orriePluginSettings .orriePluginServer th, .TwitchStreamPanel.orriePluginSettings .orriePluginServer td {font-size: 14px; text-align: center;}
-.TwitchStreamPanel.orriePluginSettings .orriePluginTable .stream_form td:first-of-type {white-space: nowrap;}
-.TwitchStreamPanel.orriePluginSettings .orriePluginTable .stream_form td:last-of-type {width: 100%;}
-.TwitchStreamPanel.orriePluginSettings .orriePluginTable .stream_form input {width: 100%;}
+.TwitchStreamPanel.orriePluginSettings .orriePluginServer button {padding: 1px 6px;}
 .TwitchStreamPanel .toggled {display: none;}
 			`,
 			shared: `
@@ -68,7 +70,7 @@ const TwitchStreamPanel = (function() {
 .orriePluginSettings .orriePluginTable table {width: 100%;}
 .orriePluginSettings .orriePluginTable td {vertical-align: middle;}
 .orriePluginSettings .orriePluginTable input[type=checkbox] {-webkit-appearance: none; border: 2px solid #CDCDCD; border-color: hsla(0,0%,100%,.2); border-radius: 3px; cursor: pointer; height: 18px; width: 18px; position: relative; -webkit-transition: .15s;}
-.orriePluginSettings .orriePluginTable input[type=checkbox]:checked {background-color: #3A71C1; border: none;}
+.orriePluginSettings .orriePluginTable input[type=checkbox]:checked {background-color: #7289DA; border: none;}
 .orriePluginSettings .orriePluginTable input[type=checkbox]:before, .orriePluginSettings .orriePluginTable input[type=checkbox]:checked:before {color: #FFFFFF; position: absolute; top: 0; left: 0; height: 100%; width: 100%; line-height: 100%; text-align: center;}
 .orriePluginSettings .orriePluginTable input[type=checkbox]:checked:before {content: '✔'; line-height: unset;}
 .orriePluginSettings .orriePluginTable input[type=range]:focus {outline: none;}
@@ -77,9 +79,10 @@ const TwitchStreamPanel = (function() {
 .orriePluginSettings .orriePluginTable input[type=range]:focus::-webkit-slider-runnable-track {background: #787C84;}
 .orriePluginSettings .orriePluginTable input[type=range]::-webkit-slider-thumb {-webkit-appearance: none; background: #45484E; border: 2px solid #CFD8DC; border-radius: 3px; cursor: pointer; height: 16px; margin-top: -6px; width: 8px;}
 .orriePluginSettings .orriePluginTable input[type=text] {color: #B0B6B9; background: inherit; border: 2px solid #CDCDCD; border-color: hsla(0,0%,100%,.2); border-radius: 3px; padding: 0 2px;}
-.orriePluginSettings .orriePluginFooter {border-top: 1px solid #3F4146; font-size: 12px; font-weight: 700; margin-bottom: 5px; padding-top: 5px; display: flex; justify-content: space-around;}
+.orriePluginSettings .orriePluginFooter {border-top: 1px solid #3F4146; font-size: 12px; font-weight: 700; margin-bottom: 5px; padding-top: 5px;}
 .orriePluginSettings .orriePluginNotice {text-align: center;}
-.orriePluginSettings button {background: #3A71C1; color: #FFFFFF; border-radius: 5px;}
+.orriePluginSettings .orriePluginFlex {display: flex; justify-content: space-around;}
+.orriePluginSettings button {background: #7289DA; color: #FFFFFF; border-radius: 5px; padding: 2px 16px;}
 .orriePluginSettings button.warning {background: #F04747;}
 .orriePluginSettings button a {color: #FFFFFF;}
 .theme-dark .orriePluginSettings {color: #B0B6B9;}
@@ -247,33 +250,32 @@ const TwitchStreamPanel = (function() {
 		settingType = function(key, props) {
 			switch(props[1]) {
 				case "check":
-					return _createElement("tr", {innerHTML: `<td><label for='id_${key}'>${props[0]}</label></td><td><input id='id_${key}' name='${key}' type='checkbox'${script.settings[key] ? " checked=checked" : ""} onchange='BdApi.getPlugin("${script.name}").settingsSave("${key}", this.checked)'/></td><td>${props[2]}</td>`});
+					return `<td><label for='id_${key}'>${props[0]}</label></td><td><input id='id_${key}' name='${key}' type='checkbox'${script.settings[key] ? " checked=checked" : ""} onchange='BdApi.getPlugin("${script.name}").settingsSave("${key}", this.checked)'/></td><td>${props[2]}</td>`;
 				case "range":
-					return _createElement("tr", {innerHTML: `<td><label for='id_${key}'>${props[0]}</label></td><td><input id='id_${key}' name='${key}' value='${script.settings[key]}' type='range' min='0' max='1' step='0.05' onchange='BdApi.getPlugin("${script.name}").settingsSave("${key}", this.value)'/></td><td>${props[2]}</td>`});
+					return `<td><label for='id_${key}'>${props[0]}</label></td><td><input id='id_${key}' name='${key}' value='${script.settings[key]}' type='range' min='0' max='1' step='0.05' onchange='BdApi.getPlugin("${script.name}").settingsSave("${key}", this.value)'/></td><td>${props[2]}</td>`;
 				case "text":
-					return _createElement("tr", {innerHTML: `<td><label for='id_${key}'>${props[0]}</label></td><td><input id='id_${key}' name='${key}' value='${script.settings[key]}' type='text' onchange='BdApi.getPlugin("${script.name}").settingsSave("${key}", this.value)'/></td><td>${props[2]}</td>`});
+					return `<td><label for='id_${key}'>${props[0]}</label></td><td><input id='id_${key}' name='${key}' value='${script.settings[key]}' type='text' onchange='BdApi.getPlugin("${script.name}").settingsSave("${key}", this.value)'/></td><td>${props[2]}</td>`;
 				default:
 					return "";
 			}
 		};
 		for (let _s_k = Object.keys(script.settingsMenu), _s=0, _s_len=_s_k.length; _s<_s_len; _s++) {
-			settingsFragment.appendChild(settingType(_s_k[_s], script.settingsMenu[_s_k[_s]]));
+			settingsFragment.appendChild(_createElement("tr", {innerHTML: settingType(_s_k[_s], script.settingsMenu[_s_k[_s]])}));
 		}
 		return _createElement("div", {className: `${script.file} orriePluginSettings`}, [
 			_createElement("div", {className: "orriePluginTable"}, [
 				_createElement("table", "", settingsFragment)
 			]),
-			_createElement("div", {className: "orriePluginForm orriePluginTable"}, [
+			_createElement("div", {className: "orriePluginAddStream orriePluginTable"}, [
 					_createElement("div", {className: "orriePluginHeader", innerHTML: `<svg class='iconDefault-xzclSQ iconTransition-VhWJ85$ closed-2Hef-I' width='12' height='12' viewBox='0 0 24 24'><path fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' d='M7 10L12 15 17 10'></path></svg><button>Add New Stream</button>`,
 						onclick() {
 							this.nextElementSibling.classList.toggle("toggled"); this.firstElementChild.classList.toggle("closed-2Hef-I");
 						}
 					}),
-					_createElement("form", {className: "stream_form toggled", name: "insert_stream", innerHTML: `<table><tr><td>Discord Name</td><td><input type="text" name="discord_name" placeholder="Required -- Can be anything"></td></tr><tr><td>Twitch Username</td><td><input type="text" name="twitch_name" placeholder="Required"></td></tr><tr><td>Discord ID</td><td><input type="text" name="discord_id" placeholder="For BetterRoleColors -- Use Discord developer mode, right click the user and copy ID"></td></tr><tr><td>Icon</td><td><input type="text" name="icon" placeholder="Will be resized to 20x20"></td></tr><tr><td>Server to Hook (ID)</td><td><input type="text" name="server_id" placeholder="Required -- Use Discord developer mode, right click the server icon and copy ID"></td></tr></table><button onclick='BdApi.getPlugin("${script.name}").saveStream()'>Add to List</button>`}),
-					_createElement("span", {id: "saveStreamInfo"})
+					_createElement("div", {className: "orriePluginStreamInput toggled", id: "saveStreamInput", innerHTML: `<table><tr><td>Discord Name</td><td><input type="text" name="discord_name" placeholder="Required -- Can be anything"></td></tr><tr><td>Twitch Username</td><td><input type="text" name="twitch_name" placeholder="Required"></td></tr><tr><td>Discord ID</td><td><input type="text" name="discord_id" placeholder="For BetterRoleColors -- Use Discord developer mode, right click the user and copy ID"></td></tr><tr><td>Icon</td><td><input type="text" name="icon" placeholder="Will be resized to 20x20"></td></tr><tr><td>Server to Hook (ID)</td><td><input type="text" name="server_id" placeholder="Required -- Use Discord developer mode, right click the server icon and copy ID"></td></tr></table><div class='orriePluginStreamFooter orriePluginFlex'><button onclick='BdApi.getPlugin("${script.name}").saveStream()'>Add to List</button><span id='saveStreamInfo'></span></div>`})
 			]),
-			_createElement("div", {className: "orriePluginStreamlist orriePluginTable", id: "twitchStreamList"}, createSettingsServerList()),
-			_createElement("div", {className: "orriePluginFooter", innerHTML: `<button><a href='${script.discord}' target='_blank' rel='noreferrer'>Support (Discord)</a></button><button><a href='${script.url}' target='_blank' rel='noreferrer'>Updates</a></button><button class='warning' onclick='BdApi.getPlugin(\"${script.name}\").cleanDB(this)'>Clean Database (Irreversible!)</button>`})
+			_createElement("div", {className: "orriePluginStreamList orriePluginTable", id: "twitchStreamList"}, createSettingsServerList()),
+			_createElement("div", {className: "orriePluginFooter orriePluginFlex", innerHTML: `<button><a href='${script.discord}' target='_blank' rel='noreferrer'>Support (Discord)</a></button><button><a href='${script.url}' target='_blank' rel='noreferrer'>Updates</a></button><button class='warning' onclick='BdApi.getPlugin(\"${script.name}\").cleanDB(this)'>Clean Database (Irreversible!)</button>`})
 		]);
 	},
 	createSettingsServerList = function () {
@@ -351,7 +353,7 @@ const TwitchStreamPanel = (function() {
 		}
 		// save stream
 		saveStream() {
-			const inputs = document.forms.insert_stream.getElementsByTagName("input"),
+			const inputs = document.getElementById("saveStreamInput").getElementsByTagName("input"),
 			streamStatus = document.getElementById("saveStreamInfo"),
 			data = [];
 			for (let _i=0, _i_len = inputs.length; _i<_i_len; _i++) {
@@ -359,22 +361,30 @@ const TwitchStreamPanel = (function() {
 			}
 			if (data[0] && data[1] && data[4]) {
 				if (BDfunctionsDevilBro.getDivOfServer(data[4])) {
+					const twitchStreamList = document.getElementById("twitchStreamList");
 					if (!script.streams[data[4]]) {
 						script.streams[data[4]] = {};
 					}
 					script.streams[data[4]][data[1]] = data.splice(0,4);
-					const twitchStreamList = document.getElementById("twitchStreamList");
+					bdPluginStorage.set(script.file, "streams", script.streams);
 					twitchStreamList.innerHTML = "";
 					twitchStreamList.appendChild(createSettingsServerList());
-					bdPluginStorage.set(script.file, "streams", script.streams);
+					streamStatus.classList.add("itemBrand-mC9YR4");
+					streamStatus.textContent = "Saved!";
 				}
 				else {
+					streamStatus.classList.add("itemDanger-3m3dwx");
 					streamStatus.textContent = "Server doesn't exist in your serverlist";
 				}
 			}
 			else {
-				streamStatus.textContent = "Missing required data (*)";
+				streamStatus.classList.add("itemDanger-3m3dwx");
+				streamStatus.textContent = "Missing required data";
 			}
+			setTimeout(function() {
+				streamStatus.className = "";
+				streamStatus.textContent = "";
+			}, 2500);
 		}
 		// clean database
 		cleanDB(elem) {
